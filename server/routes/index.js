26376f3137f00f19e6913e8f22adf336db682096
hjy <heojungyeon.97@gmail.com>
2022-07-05 17:24:40 +0900

    Feat: 가계부 상세보기 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const list = require("./accountBook/list");
 const deletedList = require("./accountBook/deletedList");
 const restore = require("./accountBook/restore");
+const detail = require("./accountBook/detail");
 
 router.use("/accountBook/list", list);
 router.use("/accountBook/deletedList", deletedList);
 router.use("/accountBook/restore", restore);
+router.use("/accountBook/detail", detail);
 
 module.exports = router;
