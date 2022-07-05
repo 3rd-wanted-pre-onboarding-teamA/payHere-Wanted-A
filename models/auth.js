68aeb7fa50b363eeac7ba5c024e520ee5423289d
username <greenrock46@gmail.com>
2022-07-05 20:13:42 +0900

    Feat: 로그인 api 로직 구현 * server/routes/auth/login.js: 사용자 로그인 로직 및 토큰 발급 api 로직 구현 * server/routes/index.js: login 라우트 등록 * models/auth.js: 로그인에 필요한 메소드 추가

--- models/auth.js
+++ models/auth.js
@@ -1,5 +1,15 @@
 const pool = require("../DB/config");
 
+/**
+ * @code writer 장덕수
+ * @description 사용자 인증 시 사용되는 sql구문 클래스로 정의
+ *
+ * @auth.signUp 회원가입 메소드
+ * @auth.checkId 아이디 중복 확인 메소드
+ * @auth.checkUser 로그인 시 유저 정보 조회 메소드
+ * 
+ * @returns sql구문으로 인한 DB 생성 및 조회
+ */
 class auth {
   // 회원가입 메소드
   static async signUp(member_id, member_pw, member_name, phone_number) {
@@ -18,6 +28,20 @@ class auth {
   // 아이디 중복 확인 메소드
   static async checkId(member_id) {
     const sql = `SELECT member_id FROM member WHERE member_id = '${member_id}'`;
+    try {
+      const connection = await pool.getConnection(async (conn) => conn);
+      const [result] = await connection.query(sql);
+      connection.release();
+      return result;
+    }
+    catch (err) {
+      throw err;
+    }
+  }
+
+  // 로그인 시 유저 정보 조회 메소드
+  static async checkUser(member_id) {
+    const sql = `SELECT member_id, member_pw FROM member WHERE member_id = '${member_id}'`;
     try {
       const connection = await pool.getConnection(async (conn) => conn);
       const [result] = await connection.query(sql);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const signUP = require("./auth/signUp");
+const login = require("./auth/login");
+const token = require("./auth/token");
 
 const router = express.Router();
 
 router.use("/auth", signUP);
+router.use("/auth", login);
+router.use("/auth", token);
 
 module.exports = router;
