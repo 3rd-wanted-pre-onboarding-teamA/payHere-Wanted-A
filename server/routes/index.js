8492fef514ff41775f08a5868c7529534b9790dc
dltmddus1998 <dltmddus1998@naver.com>
2022-07-06 09:23:29 +0900

    Feat: 가계부 관련 라우트 설정

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const create = require('./accountBook/create.js');
-
-module.exports = router;
+const modify = require('./accountBook/modify.js');
+const remove = require('./accountBook/remove.js');
 
 // 가계부 생성
-router.use("/accoutBook", create);
+router.use("/accountBook/createAction", create);
+
+// 가계부 수정
+router.use("/accountBook/updateAction", modify);
+
+// 가계부 삭제 (to 삭제 리스트)
+router.use("/accountBook/delete", remove);
+
+module.exports = router;
