66a0328c375c5c86302d5a6e0447c3877bba4127
dltmddus1998 <dltmddus1998@naver.com>
2022-07-06 09:22:44 +0900

    Feat: api 구현 -  가계부 생성

--- server/routes/accountBook/create.js
+++ server/routes/accountBook/create.js
@@ -1,34 +1,29 @@
 const pool = require('../../../DB/config.js');
 const express = require('express');
 const router = express.Router();
+const { getById } = require('../../../data/accountBook.js')
 
-// 사용자 아이디(이메일)에 따른 가계부 게시물 1개 조회하기 (join 사용)
-const SELECT_JOIN = 
-    'SELECT ac.account_book_id, ac.member_id, ac.type, ac.amount, ac.state, ac.memo, m.name FROM account_book as ac JOIN member as m ON ac.member_id=m.member_id';
-// 내림차순 조회하기 위함
-const ORDER_DESC = 'ORDER BY ac.date DESC';
-
-const getById = async id => {
-    return pool
-        .execute(`${SELECT_JOIN} WHERE ac.account_book_id=?`, [id])
-        .then(result => result[0][0]);
-}
+/**
+ * @code writer 이승연
+ * @description 가계부 등록 구현
+ *
+ * @POST ("/accountBook/createAction")
+ *
+ * @returns json
+ */
 
 router.post("/", async (req, res) => {
     let sendData = {};
     let member_id = 'tester2@test.com';
-    const { type, amount, purpose, payment, memo, state} = req.body;
+    const { type, amount, purpose, payment, memo} = req.body;
 
     try {
         const connection = await pool.getConnection(async conn => conn);
 
         try {
-            const sql = `INSERT INTO account_book (member_id, type, amount, purpose, payment, memo, state) VALUES ?`;
-            const contents = req.body;
-            console.log(contents);
+            const sql = `INSERT INTO account_book (member_id, type, amount, purpose, payment, memo) VALUES ?`;
             const values = [
-                [member_id, type, amount, purpose, payment, memo,
-                state]
+                [member_id, type, amount, purpose, payment, memo]
             ];
             const [row] = await connection.query(sql, [values]);
             sendData.create = row;
@@ -38,7 +33,7 @@ router.post("/", async (req, res) => {
         } finally {
             connection.release();
             res.status(201).json({ message: "가계부 작성 완료!" })
-            res.render("create.ejs", sendData);
+            // return res.render("create.ejs", sendData);
         }
     } catch (err) {
         console.log(err);
