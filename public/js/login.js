704bb869f3e4abdd0d4bd6f51cf1b5ffb65724fa
username <greenrock46@gmail.com>
2022-07-07 09:38:24 +0900

    Feat: 프론트단 파일 구현 중

---
+++ public/js/login.js
@@ -0,0 +1,35 @@
+// 로그인 클릭 동작
+function clickLogin() {
+  submitLogin().then(result => {
+    alert(result.message);
+    localStorage.setItem("access-token", result.accessToken);
+    localStorage.setItem("refresh-token", result.refreshToken);
+  });
+}
+
+// 로그인 제출
+async function submitLogin() {
+  try {
+    const data = {
+      member_id: document.getElementById("member_id").value,
+      member_pw: document.getElementById("member_pw").value,
+    }
+
+    const opt = {
+      method: "POST",
+      headers : {"Content-Type" : "application/json"},
+      body : JSON.stringify(data)
+    };
+
+    const submit = await fetch("/auth/loginAction", opt);
+    const result = await submit.json();
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+
+// 회원가입 창으로 이동
+function moveSignUp() {
+  location.href = "/auth/newUser";
+}
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,57 @@
-//아이디 중복 검사
-function checkId() {
-  location.href = "/auth/checkIdAction";
+//아이디 중복 검사 클릭 동작
+function clickCheckId() {
+  checkIdAction().then(message => {
+    alert(message);
+  });
+}
+
+// 아이디 중복 검사 실행 함수
+async function checkIdAction() {
+  try {
+    const data = {
+      member_id : document.getElementById("member_id").value
+    }
+    const opt = {
+      method: "POST",
+      headers : {"Content-Type" : "application/json"},
+      body: JSON.stringify(data)
+    }
+    const checkIdValid = await fetch("/auth/checkIdAction", opt);
+    const result = await checkIdValid.json();
+    return result.message;
+  } catch (err) {
+    throw err;
+  }
+}
+
+// 회원가입 클릭 동작
+function clickSignUp() {
+  submitSignUp().then(message => {
+    alert(message);
+  });
 }
 
 // 회원가입 제출
-function submitSignUp() {
-  location.href = "/auth/newUserAction";
+async function submitSignUp() {
+  try {
+    const data = {
+      member_id: document.getElementById("member_id").value,
+      member_pw: document.getElementById("member_pw").value,
+      member_name: document.getElementById("member_name").value,
+      phone_number: document.getElementById("phone_number").value,
+      balance: document.getElementById("balance").value
+    }
+
+    const opt = {
+      method: "POST",
+      headers : {"Content-Type" : "application/json"},
+      body : JSON.stringify(data)
+    };
+
+    const submit = await fetch("/auth/newUserAction", opt);
+    const result = await submit.json();
+    return result.message;
+  } catch (err) {
+    throw err;
+  }
 }
