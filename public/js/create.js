57c431bf865f8280cf6a0ae4207e0c4e365b6c6b
dltmddus1998 <dltmddus1998@naver.com>
2022-07-08 13:36:44 +0900

    Feat: 가계부 view관련 메서드 생성

---
+++ public/js/create.js
@@ -0,0 +1,44 @@
+function clickCreate() {
+    createAccoutBook()
+        .then(result => {
+            console.log(result);
+            alert(result.message);
+        });
+    location.href = "/accountBook/list";
+}
+
+async function createAccoutBook() {
+    try {
+        const data = {
+            type: "지출",
+            amount: document.getElementById("amount").value,
+            purpose: document.getElementById("purpose").value,
+            payment: "카드",
+            memo: document.getElementById("memo").value
+        };
+
+        const opt = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        };
+
+        const create = await fetch("/accountBook/createAction", opt);
+        const result = await create.json();
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+// async function createAccoutBook(id) {
+//     try {
+//         const receiveData = await fetch(`/accountBook/createAction?id=${id}`);
+//         const data = await receiveData.json();
+//         console.log(data);
+//     } catch (err) {
+//         throw err;
+//     }
+// }
---
+++ public/js/update.js
@@ -0,0 +1,42 @@
+function update() {
+    // console.log(11111111);
+    
+    updateAccoutBook()
+        .then(result => {
+            console.log("되냐????", result);
+            alert(result.message);
+        });
+    location.href = "/accountBook/list";
+}
+
+async function updateAccoutBook() {
+    // console.log(22222222);
+    try {
+        // console.log(3333333333);
+        const data = {
+            account_book_id: document.getElementById("account_book_id").value,
+            type: "지출",
+            amount: document.getElementById("payment").value,
+            purpose: document.getElementById("purpose").value,
+            payment: "현금",
+            memo: document.getElementById("memo").value,
+        };
+        console.log(data);
+
+        const opt = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        };
+
+        const update = await fetch("/accoutBook/updateAction", opt);
+        console.log(update);
+        const result = await update.json();
+        console.log(result);
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
