ac06a95836f42cb4f551a26de534d97ce716bf74
syoungee <sunyoungah69@gmail.com>
2022-07-08 18:22:10 +0900

    applied prettier

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 function logout() {
-  submitLogout().then(message => {
+  submitLogout().then((message) => {
     alert(message);
     if (message === "로그아웃 되었습니다.") {
       location.href = "/auth/login";
@@ -10,11 +10,7 @@ function logout() {
 async function submitLogout() {
   try {
     const submit = await fetch("/auth/logout");
-    console.log(submit);
-    console.log(typeof submit);
     const result = await submit.json();
-    console.log(result);
-    console.log(typeof result);
     return result.message;
   } catch (err) {
     throw err;
