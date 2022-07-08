e3bbd9f67f6b4fe94a011b9cbd93400f2c615cf6
dltmddus1998 <dltmddus1998@naver.com>
2022-07-08 12:07:54 +0900

    Feat: 리스트 페이지에 수정하기,삭제하기 버튼 메서드 적용

--- public/js/list.js
+++ public/js/list.js
@@ -2,3 +2,14 @@
 function detail(id) {
   location.href = "/accountBook/detail?id=" + id;
 }
+
+// 수정하기로 이동
+function update(id) {
+  // console.log("aaa");
+  location.href = "/accountBook/update?id=" + id;
+}
+
+// 삭제하기
+function remove(id) {
+  location.href
+}
