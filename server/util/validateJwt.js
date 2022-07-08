951324a4d2fb2394ef6a2f5806f10ed00071a7f1
username <greenrock46@gmail.com>
2022-07-09 01:20:31 +0900

    upgrade: 엑세스 토큰 재발급 로직 수정 변경 사항: authenticateAccessToken( JWT유효성 검증 메소드)에서 만료된 토큰 재발급하도록 로직 수정   - 만료되었다는 메시지를 클라이언트에게 보내기 전에 먼저 토큰 재발급

--- server/util/validateJwt.js
+++ server/util/validateJwt.js
@@ -1,5 +1,7 @@
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
+const AuthService = require("../services/auth.service");
+const { generateAccessToken } = require("../util/generateToken");
 
 dotenv.config();
 
@@ -11,7 +13,11 @@ dotenv.config();
  */
 const authenticateAccessToken = (req, res, next) => {
   const authHeader = req.headers["cookie"];
-  const token = authHeader.replace("access-token=", "");
+  if (authHeader === undefined) {
+    res.status(403).send("로그인이 필요합니다.");
+  }
+
+  const token = authHeader.replace("access-token=", "");  // 쿠키에 있는 엑세스 토큰 추출
 
   if (!token) {
       console.log("wrong token format or token is not sended");
@@ -20,14 +26,37 @@ const authenticateAccessToken = (req, res, next) => {
       });
   }
 
-  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (error, user) => {
-      if (error) {
-          console.log(error);
-          return res.sendStatus(403);
+  
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, async (error, user) => {
+    if (error) {    // 엑세스 토큰이 만료되었다면 리프레시 토큰을 활용해 엑세스 토큰 재발급
+      // access 토큰 디코딩하여 user 정보 조회
+      const decoded = jwt.decode(token);
+    
+      // 디코딩 결과가 없으면 권한 없음 응답
+      if (decoded === null) {
+        return res.status(401).send({
+          message: "해당 토큰이 권한이 없습니다."
+        });
       }
-      
+    
+      // 디코딩된 값에서 유저 id 가져와 리프레시 토큰 검증
+      const refreshToken = await AuthService.searchRefreshToken(decoded.id);
+      if (!refreshToken) return res.sendStatus(401);
+    
+      jwt.verify(refreshToken[0].refresh_token, process.env.JWT_REFRESH_TOKEN, (error, user) => {
+        if (error) {
+          return res.status(403).send("로그인이 필요합니다.");
+        }
+        const accessToken = generateAccessToken(user.id);
+        res.setHeader("Authorization", "Bearer" + accessToken);
+        res.cookie("access-token", accessToken);
+        req.user = user;
+        next();
+      });
+    } else {
       req.user = user;
       next();
+    }
   });
 };
 
