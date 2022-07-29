c51ab0be381559fe4d32498d5406faf5d4e02519
username <greenrock46@gmail.com>
2022-07-29 09:37:22 +0900

    Refactor: 테스트 코드 수정 가계부 관련 테스트 케이스 영어 -> 한글 수정

--- server/test/accountBook.spec.js
+++ server/test/accountBook.spec.js
@@ -2,7 +2,7 @@ const httpMocks = require("node-mocks-http");
 const AccountBookController = require("../controllers/accountBook.controller.js");
 
 // 가계부 생성
-describe("create account book", () => {
+describe("가계부 생성", () => {
   let type, amount, purpose, payment, memo, use_date, request, response;
   beforeEach(() => {
     type = "지출";
@@ -27,14 +27,14 @@ describe("create account book", () => {
     response = httpMocks.createResponse();
   });
 
-  it("create account book", async () => {
+  it("가계부 생성 완료", async () => {
     await AccountBookController.createAccoutBook(request, response);
     expect(response.statusCode).toBe(201);
   });
 });
 
 // 가계부 수정 form
-describe("update account book form", () => {
+describe("가계부 수정 form", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       query: {
@@ -43,14 +43,14 @@ describe("update account book form", () => {
     });
     response = httpMocks.createResponse();
   });
-  it("update account book form", async () => {
+  it("가계부 수정 form 완료", async () => {
     await AccountBookController.update(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 가계부 수정 동작
-describe("update account book", () => {
+describe("가계부 수정", () => {
   let type, amount, purpose, payment, memo, use_date, request, response;
   beforeEach(() => {
     account_book_id = 3;
@@ -74,14 +74,14 @@ describe("update account book", () => {
     response = httpMocks.createResponse();
   });
 
-  it("update account book", async () => {
+  it("가계부 수정 동작 완료", async () => {
     await AccountBookController.updateAccountBook(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 가계부 삭제
-describe("delete account book", () => {
+describe("가계부 삭제 성공시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       query: {
@@ -91,27 +91,27 @@ describe("delete account book", () => {
     response = httpMocks.createResponse();
   });
 
-  it("delete account book", async () => {
+  it("가계부 삭제 완료", async () => {
     await AccountBookController.deleteAccoutBook(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 가계부 삭제 - querystring 없을 때
-describe("delete account book not querystring", () => {
+describe("가계부 삭제 실패시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest();
     response = httpMocks.createResponse();
   });
 
-  it("delete account book not querystring", async () => {
+  it("가계부 삭제 querystring 존재하지 않음", async () => {
     await AccountBookController.deleteAccoutBook(request, response);
     expect(response.statusCode).toBe(404);
   });
 });
 
 // 가계부 목록 조회
-describe("select account book list", () => {
+describe("가계부 목록 조회", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       user: {
@@ -121,14 +121,14 @@ describe("select account book list", () => {
     response = httpMocks.createResponse();
   });
 
-  it("select account book list", async () => {
+  it("가계부 목록 조회 완료", async () => {
     await AccountBookController.getAccountBookList(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 삭제된 가계부 목록 조회
-describe("select account book deleted list", () => {
+describe("삭제된 가계부 목록 조회", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       user: {
@@ -138,14 +138,14 @@ describe("select account book deleted list", () => {
     response = httpMocks.createResponse();
   });
 
-  it("select account book deleted list", async () => {
+  it("삭제된 가계부 목록 조회 완료", async () => {
     await AccountBookController.getAccountBookDeletedList(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 상세보기 조회
-describe("select account book detail", () => {
+describe("상세보기 조회 성공 시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       query: {
@@ -155,27 +155,27 @@ describe("select account book detail", () => {
     response = httpMocks.createResponse();
   });
 
-  it("select account book detail", async () => {
+  it("상세보기 조회 완료", async () => {
     await AccountBookController.getAccountBookDetail(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 상세보기 조회 - querystring 없을 때
-describe("select account book detail not querystring", () => {
+describe("상세보기 조회 실패 시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest();
     response = httpMocks.createResponse();
   });
 
-  it("select account book detail not querystring", async () => {
+  it("상세보기 조회 querystring 존재하지 않음", async () => {
     await AccountBookController.getAccountBookDetail(request, response);
     expect(response.statusCode).toBe(404);
   });
 });
 
 // 복원하기 
-describe("restore account book", () => {
+describe("가계부 복원 성공 시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest({
       query: {
@@ -185,20 +185,20 @@ describe("restore account book", () => {
     response = httpMocks.createResponse();
   });
 
-  it("restore account book", async () => {
+  it("가계부 복원 완료", async () => {
     await AccountBookController.putAccountBookRestore(request, response);
     expect(response.statusCode).toBe(200);
   });
 });
 
 // 복원하기 - querystring 없을 때
-describe("restore account book not querystring", () => {
+describe("가계부 복원 실패 시", () => {
   beforeEach(() => {
     request = httpMocks.createRequest();
     response = httpMocks.createResponse();
   });
 
-  it("restore account book not querystring", async () => {
+  it("가계부 복원 querystring 존재하지 않음", async () => {
     await AccountBookController.putAccountBookRestore(request, response);
     expect(response.statusCode).toBe(404);
   });
